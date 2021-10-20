2fece3ac2232acc5a0b7d3546792dc7f92867ea8
WilberEnriqueSeguraRamirez <71159717+WilberEnriqueSeguraRamirez@users.noreply.github.com>
2021-10-19 19:58:17 -0600

    MDI Contabilidad

---
+++ Modulos/Contabilidad/Vista/frmContabilidad.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Vista
+{
+    partial class frmContabilidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmContabilidad));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaGeneralToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasInteligentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reporteadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polizasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polizasContablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polizasDeOtrosModulosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.presupuestosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.activosFijosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadosFinancierosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cierreContableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.polizasToolStripMenuItem,
+            this.presupuestosToolStripMenuItem,
+            this.activosFijosToolStripMenuItem,
+            this.estadosFinancierosToolStripMenuItem,
+            this.cierreContableToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaGeneralToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.inicioToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // ayudaGeneralToolStripMenuItem
+            // 
+            this.ayudaGeneralToolStripMenuItem.Name = "ayudaGeneralToolStripMenuItem";
+            this.ayudaGeneralToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ayudaGeneralToolStripMenuItem.Text = "Ayuda General";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasInteligentesToolStripMenuItem,
+            this.reporteadorToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // consultasInteligentesToolStripMenuItem
+            // 
+            this.consultasInteligentesToolStripMenuItem.Name = "consultasInteligentesToolStripMenuItem";
+            this.consultasInteligentesToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
+            this.consultasInteligentesToolStripMenuItem.Text = "Consultas Inteligentes";
+            // 
+            // reporteadorToolStripMenuItem
+            // 
+            this.reporteadorToolStripMenuItem.Name = "reporteadorToolStripMenuItem";
+            this.reporteadorToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
+            this.reporteadorToolStripMenuItem.Text = "Reporteador";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // polizasToolStripMenuItem
+            // 
+            this.polizasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.polizasContablesToolStripMenuItem,
+            this.polizasDeOtrosModulosToolStripMenuItem});
+            this.polizasToolStripMenuItem.Name = "polizasToolStripMenuItem";
+            this.polizasToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.polizasToolStripMenuItem.Text = "Polizas";
+            // 
+            // polizasContablesToolStripMenuItem
+            // 
+            this.polizasContablesToolStripMenuItem.Name = "polizasContablesToolStripMenuItem";
+            this.polizasContablesToolStripMenuItem.Size = new System.Drawing.Size(260, 26);
+            this.polizasContablesToolStripMenuItem.Text = "Polizas Contables";
+            // 
+            // polizasDeOtrosModulosToolStripMenuItem
+            // 
+            this.polizasDeOtrosModulosToolStripMenuItem.Name = "polizasDeOtrosModulosToolStripMenuItem";
+            this.polizasDeOtrosModulosToolStripMenuItem.Size = new System.Drawing.Size(260, 26);
+            this.polizasDeOtrosModulosToolStripMenuItem.Text = "Polizas de Otros Modulos";
+            // 
+            // presupuestosToolStripMenuItem
+            // 
+            this.presupuestosToolStripMenuItem.Name = "presupuestosToolStripMenuItem";
+            this.presupuestosToolStripMenuItem.Size = new System.Drawing.Size(109, 24);
+            this.presupuestosToolStripMenuItem.Text = "Presupuestos";
+            // 
+            // activosFijosToolStripMenuItem
+            // 
+            this.activosFijosToolStripMenuItem.Name = "activosFijosToolStripMenuItem";
+            this.activosFijosToolStripMenuItem.Size = new System.Drawing.Size(105, 24);
+            this.activosFijosToolStripMenuItem.Text = "Activos Fijos";
+            // 
+            // estadosFinancierosToolStripMenuItem
+            // 
+            this.estadosFinancierosToolStripMenuItem.Name = "estadosFinancierosToolStripMenuItem";
+            this.estadosFinancierosToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.estadosFinancierosToolStripMenuItem.Text = "Estados Financieros";
+            // 
+            // cierreContableToolStripMenuItem
+            // 
+            this.cierreContableToolStripMenuItem.Name = "cierreContableToolStripMenuItem";
+            this.cierreContableToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
+            this.cierreContableToolStripMenuItem.Text = "Cierre Contable";
+            // 
+            // frmContabilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmContabilidad";
+            this.Text = "7001 Contabilidad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaGeneralToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasInteligentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reporteadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polizasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polizasContablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polizasDeOtrosModulosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem presupuestosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem activosFijosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadosFinancierosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cierreContableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
